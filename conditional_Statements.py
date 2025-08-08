65e716ec3d7987acd5b06ab6995f4fc775dd911f
linukumarkv <linukumar.kv1983@gmail.com>
2025-08-08 07:28:52 -0700

    conditional statments

---
+++ conditional_Statements.py
@@ -0,0 +1,14 @@
+"""  if condition:
+        statements
+        """""
+
+age = int( input("Enter the age:  "))
+
+if age >=18 and age<=70 :
+    print("Eligible")
+
+elif age >70:
+    print("Not Eligible above 70")
+
+else:
+    print("Not Eligible - Underage")
